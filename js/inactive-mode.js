389d09fbbc70d405522e822959e9fe01119285a1
GrandEnfant <37689523+GrandEnfant@users.noreply.github.com>
2020-06-24 13:19:07 +0300

    eslint

--- js/inactive-mode.js
+++ js/inactive-mode.js
@@ -3,7 +3,7 @@
 (function () {
   window.fieldset = document.querySelector('fieldset');
   window.adForm = document.querySelector('.ad-form');
-  fieldset.disabled = true;
+  window.fieldset.disabled = true;
   window.adFormMapFilters = document.querySelectorAll('.map__filter');
   window.addressField.placeholder = parseInt(window.mapPinMain.style.left, 10) + ', ' + parseInt(window.mapPinMain.style.top, 10);
 }());
--- js/main.js
+++ js/main.js
@@ -4,14 +4,14 @@ window.mapPinMain = document.querySelector('.map__pin--main');
 window.addressField = document.querySelector('#address');
 window.map = document.querySelector('.map');
 
-mapPinMain.addEventListener('mousedown', function (evt) {
+window.mapPinMain.addEventListener('mousedown', function (evt) {
   if (evt.button === 0) {
     window.applyActive();
-    addressField.placeholder = evt.x + ', ' + evt.y;
+    window.addressField.placeholder = evt.x + ', ' + evt.y;
   }
 });
 
-mapPinMain.addEventListener('keydown', function (evt) {
+window.mapPinMain.addEventListener('keydown', function (evt) {
   if (evt.code === 'Enter') {
     window.applyActive();
   }
