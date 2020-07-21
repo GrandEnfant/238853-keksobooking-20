1c66b7202608181d50d5d43a762f8d2d2ab3b002
GrandEnfant <37689523+GrandEnfant@users.noreply.github.com>
2020-07-21 14:14:35 +0300

    added ads filter

--- js/main.js
+++ js/main.js
@@ -2,13 +2,17 @@
 
 var ADS_NUMBER = 5;
 var url = 'https://javascript.pages.academy/keksobooking/data';
-var housingType = document.querySelector('#housing-type');
 var pinNode = document.querySelector('.map__pin--main');
 var forms = document.querySelector('.ad-form');
+var filtersContainer = document.querySelector('.map__filters-container');
+var filtersNode = document.querySelector('.map__filters');
+var filterSelectors = filtersNode.querySelectorAll('select');
+var inputSelectors = filtersNode.querySelectorAll('input');
 var pinCoordinateInit = {
   x: pinNode.style.left,
   y: pinNode.style.top,
 };
+var DEBOUNCE_INTERVAL = 500;
 var submitButton = document.querySelector('.ad-form__submit');
 var pinCoordinateString = window.map.getCoordinate(pinCoordinateInit);
 window.form.setDisable(true);
@@ -22,7 +26,34 @@ var fixCoordinates = function (evt) {
   window.form.fillAddress(pinCoordinateString);
   pinNode.removeEventListener('mousedown', fixCoordinates);
 };
-
+var addEventOnPins = function (ads) {
+  var renderedPins = document.querySelectorAll('.rendered-pin');
+  renderedPins.forEach(function (item) {
+    item.addEventListener('click', function (evt) {
+      if (evt.button === 0) {
+        var id = evt.currentTarget.id;
+        var dataCard = window.card.generate(ads[id]);
+        window.popup.showCard(dataCard);
+      }
+    });
+    item.addEventListener('keydown', function (evt) {
+      if (evt.code === 'Enter') {
+        var id = evt.currentTarget.id;
+        var dataCard = window.card.generate(ads[id]);
+        window.popup.showCard(dataCard);
+      }
+    });
+  });
+};
+var debounce = function (cb) {
+  var lastTimeout = null;
+  if (lastTimeout) {
+    window.clearTimeout(lastTimeout);
+  }
+  lastTimeout = window.setTimeout(function () {
+    cb();
+  }, DEBOUNCE_INTERVAL);
+};
 var applyActiveMode = function () {
   window.form.setDisable(false);
   window.form.fillAddress(pinCoordinateString);
@@ -30,30 +61,26 @@ var applyActiveMode = function () {
     var adsSlice = ads.slice(0, ADS_NUMBER);
     var initAdsFragment = window.map.createAds(adsSlice, window.pin.generate);
     window.map.renderAds(initAdsFragment);
-    housingType.addEventListener('change', function () {
-      var filteredAds = window.filters.selectData(ads, housingType.value).slice(0, ADS_NUMBER);
+    addEventOnPins(ads);
+    var filterAndRenderAds = function () {
+      var filteredAds = window.filters.filterAds(ads).slice(0, ADS_NUMBER);
       var filteredAdsFragment = window.map.createAds(filteredAds, window.pin.generate);
       window.map.removePins('.rendered-pin');
       window.map.renderAds(filteredAdsFragment);
-    });
-    var renderedPins = document.querySelectorAll('.rendered-pin');
-    renderedPins.forEach(function (item) {
-      item.addEventListener('click', function (evt) {
-        if (evt.button === 0) {
-          var id = evt.currentTarget.id;
-          var dataCard = window.card.generate(ads[id]);
-          window.popup.showCard(dataCard);
-        }
+      addEventOnPins(filteredAds);
+    };
+    var addEventOnFilters = function (elem) {
+      elem.addEventListener('change', function () {
+        window.popup.close('.popup', filtersContainer);
+        debounce(filterAndRenderAds);
       });
-      item.addEventListener('keydown', function (evt) {
-        if (evt.code === 'Enter') {
-          var id = evt.currentTarget.id;
-          var dataCard = window.card.generate(ads[id]);
-          window.popup.showCard(dataCard);
-        }
-      });
+    };
+    inputSelectors.forEach(function (elem) {
+      addEventOnFilters(elem);
     });
-
+    filterSelectors.forEach(function (elem) {
+      addEventOnFilters(elem);
+    });
     pinNode.addEventListener('mousedown', fixCoordinates);
   },
   function () {
